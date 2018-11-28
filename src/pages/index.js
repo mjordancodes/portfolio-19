591769d0eff5e940499e8872b6407831e7ca9413
mJordan <42599299+mjordancodes@users.noreply.github.com>
2018-11-27 17:04:05 -0800

    clean up + connect with netlifycms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-
 import BackgroundImage from '../images/home-background.svg'
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <BackgroundImage />
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
