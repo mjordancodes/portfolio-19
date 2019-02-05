8eaff0697e375072fc765bb075da3e11575b11ad
mJordan <42599299+mjordancodes@users.noreply.github.com>
2019-02-04 19:24:36 -0800

    reorganizing about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout';
 import Header from '../components/Header/header';
 import SimpleText from '../components/Simple-Text/simple-text';
 
-import AboutMe from '../images/about-me.svg';
+import AboutMe from '../images/dev.svg';
 
 const { TimelineMax } = require('gsap/all');
 
@@ -32,17 +32,15 @@ class AboutPage extends Component {
               Today, I volunteer as part of the Leadership Teams for PDXNode and Women Who Code Portland. I also mentor periodically at other local meetups such as Free Code Camp Portland and Mentorship Saturdays. 
             </p>
           </SimpleText>
-          <div className={componentStyles.groupImages}>
-            <img src="/images/Group-Logos/wwcode.png" alt="Women Who Code logo" id="group-wwcode" />
-            <img src="/images/Group-Logos/pdxnode.png" alt="PDXNode logo" id="group-pdxnode" />
-            <img src="/images/Group-Logos/toastmasters.png" alt="toastmasters logo" id="group-toastmasters" />
-          </div>
-          <SimpleText title="Women Who Code: Portland" className={`wwcode ${componentStyles.group}`}>
+          <img src="/images/Group-Logos/wwcode.png" alt="Women Who Code logo" id="group-wwcode" className={componentStyles.wwcodeImg}/>
+          <img src="/images/Group-Logos/pdxnode.png" alt="PDXNode logo" id="group-pdxnode" className={componentStyles.pdxnodeImg}/>
+          <img src="/images/Group-Logos/toastmasters.png" alt="toastmasters logo" id="group-toastmasters" className={componentStyles.toastmastersImg}/>
+          <SimpleText title="Women Who Code: Portland" className={componentStyles.wwcode}>
             <p>
               During the second half of 2016 I moved from an occasional volunteer to a full member of the leadership team. In the beginning, I mostly helped out with monthly networking nights. I was even able to take lead and plan one myself. in 2018, I stepped up into a more regular role as the co-lead for the new monthly Open Soruce Study Nights. As co-lead for a study night, I schedule speakers, prepare my own content to teach, and generally answer questions for people who attend the events. 
             </p>
           </SimpleText>
-          <SimpleText title="PDXNode" className={`pdxnode ${componentStyles.group}`}>
+          <SimpleText title="PDXNode" className={componentStyles.pdxnode}>
           <p>
             I started volunteering with PDXNode in early 2017. We have two regular events every month: presentation nights and hack nighs. My role with this group includes securing sponsorship, updating meetup.com and twitter, finding and scheduling speakers, developing workshops, and occasionally acting as announcer at the events themselves.
           </p>
@@ -50,7 +48,7 @@ class AboutPage extends Component {
             During summer of 2018, I acted as lead organizer and hosted a special edition of the presntation night as part of OSCON (Open Source Conference put on by O'Reilly) and an International Nodebots Day workshop
           </p>
           </SimpleText>
-          <SimpleText title="Toastmasters" className={`toastmasters ${componentStyles.group}`}>
+          <SimpleText title="Toastmasters" className={componentStyles.toastmasters}>
           <p>
             I joined ToastMasters in February of 2018, and completed my first four speeches within the first six months. I was elected VP of Education for the West Beaverton ToastMasters club. Unfortunatly, I won't be able to serve at this time due to taking a new job that is requiring me to find a different club to join. Hopefully I will have another opportunity to fill an officer role down the line. In the mean time, I will continue to attend weekly club meetings (once I find a new club) and participate in every meeting.
           </p>
@@ -58,79 +56,6 @@ class AboutPage extends Component {
         </div>
       </Layout>
     );
-  }
-
-  componentDidMount() {
-    const wwcode = document.getElementById('group-wwcode');
-    const aboutWWCode = document.querySelector('[class*=wwcode]');
-    const accessoryWWCode = document.getElementById('wwcode-shirt');
-
-    const pdxnode = document.getElementById('group-pdxnode');
-    const aboutPDXNode = document.querySelector('[class*=pdxnode]');
-    const accessoryPDXNode = document.getElementById('pdxnode-shirt');
-
-    const toastmasters = document.getElementById('group-toastmasters');
-    const aboutToastmasters = document.querySelector('[class*=toastmasters]');
-    const accessoryToastmasters = document.getElementById('podium');
-
-    function resetStyles() {
-      aboutWWCode.style.display = 'none';
-      aboutPDXNode.style.display = 'none';
-      aboutToastmasters.style.display = 'none';
-      accessoryToastmasters.style.display = 'none';
-      accessoryPDXNode.style.display = 'none';
-      accessoryWWCode.style.display = 'none';
-    }
-
-    resetStyles();
-
-    wwcode.addEventListener('click', function() {
-      resetStyles();
-      aboutWWCode.style.display = 'block';
-      accessoryWWCode.style = '';
-    })
-
-    pdxnode.addEventListener('click', function() {
-      resetStyles();
-      aboutPDXNode.style.display = 'block';
-      accessoryPDXNode.style = '';
-    })
-
-    toastmasters.addEventListener('click', function() {
-      resetStyles();
-      aboutToastmasters.style.display = 'block';
-      accessoryToastmasters.style = '';
-    })
-
-    function spin(groupImage, isRunning) {
-      const spinAnimation = new TimelineMax();
-      if(isRunning) {
-        spinAnimation.to(groupImage, 0.5, {scaleX: -1})
-      } else {
-        spinAnimation.to(groupImage, 0.5, {scaleX: 1})
-      }
-    }
-
-    wwcode.addEventListener('mouseover', function() {
-      spin(wwcode, true);
-    });
-    wwcode.addEventListener('mouseout', function() {
-      spin(wwcode, false);
-    });
-
-    pdxnode.addEventListener('mouseover', function() {
-      spin(pdxnode, true);
-    });
-    pdxnode.addEventListener('mouseout', function() {
-      spin(pdxnode, false);
-    });
-
-    toastmasters.addEventListener('mouseover', function() {
-      spin(toastmasters, true);
-    });
-    toastmasters.addEventListener('mouseout', function() {
-      spin(toastmasters, false);
-    });
   }
 }
 
