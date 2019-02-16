da0d3e80fbe97fd8396b019e6d1e80d4f8b3c763
mJordan <hi@mjordan.codes>
2019-02-16 02:46:56 -0800

    querying for project data instead of hardcoding

--- src/pages/project/test-project.md
+++
@@ -1,9 +0,0 @@
----
-date: 2019-02-16T10:10:57.853Z
-title: TEST PROJECT
-techs:
-  - HTML
-  - bananas
-layout: project
----
-lorem ipsum
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { graphql } from "gatsby";
+
 
 import Layout from '../components/layout';
 import TechList from '../components/Tech-List/tech-list';
@@ -13,25 +15,16 @@ class ProjectsPage extends Component {
       <Layout>
         <div className={`container ${componentStyles.container}`}>
         <h1>Recent Projects</h1>
-        <ProjectCard 
-          title="Michelle and Zack Wedding"
-          live="http://michelleandzack.netlify.com/"
-          github="https://github.com/mjordancodes/Michelle-and-Zack"
-          description="I was told to go make a wedding site. So I did, but now the wedding is over and sometimes I get bored so I am continuing to be ridiculous. I built this using Gatsby. I had a lot of fun on this project using gatsby-image to set up the photo albums quickly. I also used styled-components for styling in this project."
-          image="/assets/michelleandzack.png"
-        />
-        <ProjectCard 
-          title="Beach Hillel"
-          github="https://github.com/mjordancodes/Beach-Hillel"
-          description="Newly built and designed website for the Beach Hillel group. Site is built using Gatsby. Connecting a CMS shortly."
-          image="/assets/beach-hillel.png"
-        />
-        <ProjectCard 
-          title="Girl Friend Factor"
-          image="/assets/gff.png"
-          live="https://girlfriendfactor.org/"
-          description="Girl Friend Factor is a non-profit in Southern California that helps women with educational grants. I designed and rebuilt this website updating the technology from a basic html site to using WordPress to allow for easy updates for the non-technical staff/volunteers."
-        />
+        {this.props.data.allMarkdownRemark.edges.map(project => (
+          <ProjectCard 
+            title={project.node.frontmatter.title}
+            image={project.node.frontmatter.screenshot} 
+            github={project.node.frontmatter.github} 
+            live={project.node.frontmatter.live} 
+          >
+            <div dangerouslySetInnerHTML={{ __html: project.html }} /> 
+          </ProjectCard>
+        ))}
           {/* <div className={componentStyles.github} >
             <a href="https://github.com/mjordancodes">
               <img src="/images/github.png" alt="github logo with custom octocat" />
@@ -52,5 +45,33 @@ class ProjectsPage extends Component {
   }
 }
 
+export const projectListQuery = graphql`
+  query {
+    allMarkdownRemark(
+      filter: {
+        frontmatter : {
+          layout:{
+            eq:"project"
+          }
+        }
+      }, 
+      sort: {
+        fields: [frontmatter___date], order: DESC
+      }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            screenshot
+            github
+            live
+          }
+          html
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
