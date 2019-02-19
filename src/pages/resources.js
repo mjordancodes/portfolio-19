36ad54398665e9c5c927adcf2482b899cac96a17
mJordan <hi@mjordan.codes>
2019-02-18 22:34:37 -0800

    empty resource page

---
+++ src/pages/resources.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Layout from '../components/layout';
+
+class ResourcesPage extends Component {
+  render() {
+    return (
+      <Layout>
+RESOURCES
+      </Layout>
+    );
+  }
+}
+
+
+
+export default ResourcesPage
