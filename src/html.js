618b9080528a02b15a82c584746f0a8a7779e021
mJordan <hi@mjordan.codes>
2019-07-08 09:06:52 -0700

    indie auth

--- src/html.js
+++ src/html.js
@@ -14,6 +14,8 @@ export default class HTML extends React.Component {
           />
           {this.props.headComponents}
           <link href="https://fonts.googleapis.com/css?family=Open+Sans|Poiret+One" rel="stylesheet" /> 
+          <link rel="authorization_endpoint" href="https://indieauth.com/auth" />
+          <link rel="token_endpoint" href="https://tokens.indieauth.com/token" />
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
