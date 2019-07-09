370244c934914087d03437de45c59ee978d6eea7
mJordan <hi@mjordan.codes>
2019-07-08 22:43:09 -0700

    added 100-days-of-content day 1

--- src/pages/100-days-of-content.js
+++ src/pages/100-days-of-content.js
@@ -31,6 +31,13 @@ class OneHundredDaysOfContentPage extends Component {
             <td><a href="/blog/100-days-of-content/">#100DaysOfContent</a></td>
             <td>Kicked off the challenge with a blog post that gathers up some ideas and goals to keep in mind as the days go by.</td>
           </tr>
+          <tr>
+            <td>001</td>
+            <td>July 08, 2019</td>
+            <td>Slides</td>
+            <td>Coming Soon. Will post early on the 16th.</td>
+            <td>Built slide deck for animation presentation I am giving at Front End PDX next week. While building this deck I focused on the why as well as just the technical how. I've found that spelling out the why and giving examples of the when can make a technical talk way more intersting. </td>
+          </tr>
         </table>
         
       </Layout>
