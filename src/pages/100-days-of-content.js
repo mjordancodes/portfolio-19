ef48ba7d85aeb88d4228df63f5eb0d047fd244ae
mJordan <hi@mjordan.codes>
2019-07-10 00:22:47 -0700

    #100daysofcontent #3

--- src/pages/100-days-of-content.js
+++ src/pages/100-days-of-content.js
@@ -4,7 +4,6 @@ import Layout from '../components/layout';
 
 import componentStyles from './100-days-of-content.module.css';
 
-
 class OneHundredDaysOfContentPage extends Component {
   render() {
     return (
@@ -24,6 +23,7 @@ class OneHundredDaysOfContentPage extends Component {
             <th>Link</th>
             <th>Notes / Project Summary</th>
           </thead>
+
           <tr>
             <td>000</td>
             <td>July 07, 2019</td>
@@ -31,12 +31,28 @@ class OneHundredDaysOfContentPage extends Component {
             <td><a href="/blog/100-days-of-content/">#100DaysOfContent</a></td>
             <td>Kicked off the challenge with a blog post that gathers up some ideas and goals to keep in mind as the days go by.</td>
           </tr>
+
           <tr>
             <td>001</td>
             <td>July 08, 2019</td>
             <td>Slides</td>
-            <td>Coming Soon. Will post early on the 16th.</td>
+            <td>
+              <img src="assets/animation-slides.png" alt="Title slide with title: Animating your portfolio to awesome with Greensock"/>
+              <br />
+              Slide Deck will be posted early on the 16th.</td>
             <td>Built slide deck for animation presentation I am giving at Front End PDX next week. While building this deck I focused on the why as well as just the technical how. I've found that spelling out the why and giving examples of the when can make a technical talk way more intersting. </td>
+          </tr>
+
+          <tr>
+            <td>002</td>
+            <td>July 09, 2019</td>
+            <td>Acrylic Painting</td>
+            <td>
+              <a href="/assets/jordans-journal.jpeg">
+                <img src="/assets/jordans-journal.jpeg" className={componentStyles.jordansJournal} alt="Light blue journal with paintings of Peter Pan, a blue Converse shoe, a rocket ship and two planets" />
+              </a>
+            </td>
+            <td>Decided that my notebook was too boring and single colored so </td>
           </tr>
         </table>
         
