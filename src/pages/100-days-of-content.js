934c0e578a34e49fe33b2e558e00b2b1829a5889
mJordan <hi@mjordan.codes>
2019-07-13 00:06:57 -0700

    #100content day 004, flipped table so that most recent content is on top

--- src/pages/100-days-of-content.js
+++ src/pages/100-days-of-content.js
@@ -12,7 +12,7 @@ class OneHundredDaysOfContentPage extends Component {
 
         <p>Project started on <strong>July 7th, 2019</strong>.</p>
 
-        <p>Day 0 blog post is a good place to start if you want to know more about what this project is.</p>
+        <p>Day 0 blog post (<a href="/blog/100-days-of-content/">#100DaysOfContent</a>) is a good place to start if you want to know more about what this project is.</p>
 
         <h2>Contents:</h2>
         <table className={componentStyles.table}>
@@ -25,11 +25,37 @@ class OneHundredDaysOfContentPage extends Component {
           </thead>
 
           <tr>
-            <td>000</td>
-            <td>July 07, 2019</td>
-            <td>Blog Post</td>
-            <td><a href="/blog/100-days-of-content/">#100DaysOfContent</a></td>
-            <td>Kicked off the challenge with a blog post that gathers up some ideas and goals to keep in mind as the days go by.</td>
+            <td>004</td>
+            <td>July 12, 2019</td>
+            <td>SVG Animations</td>
+            <td>
+              <a href="https://codepen.io/mjordancodes/pen/JQzQdq?editors=0010">CodePen: BeeFly</a>
+            </td>
+            <td>Took the SVG I created on day 003 and added a little animation to it. The green short plant shakes a little on load. If you bring your mouse over the plant, a bee flies out and the plant stops shaking. Will be using this as a demo in my talk on Tuesday most likely. Might add a few more parts to the animation over the weekend. Not sure.</td>
+          </tr>
+
+          <tr>
+            <td>003</td>
+            <td>July 10, 2019</td>
+            <td>SVG Illustration</td>
+            <td>
+              <a href="/assets/terrarium.png">
+                <img src="/assets/terrarium.png" alt="Graphic drawing of a few succulents in a bubble bowl" />
+              </a>
+            </td>
+            <td>Worked on a SVG of a succulent terrarium to add to the bookcase on my books list page. Want to add a bee that flies out of the bowl and around the page that is triggered by a mouse hover over. Maybe tomorrow's project.</td>
+          </tr>
+
+          <tr>
+            <td>002</td>
+            <td>July 09, 2019</td>
+            <td>Acrylic Painting</td>
+            <td>
+              <a href="/assets/jordans-journal.jpeg">
+                <img src="/assets/jordans-journal.jpeg" className={componentStyles.jordansJournal} alt="Light blue journal with paintings of Peter Pan, a blue Converse shoe, a rocket ship and two planets" />
+              </a>
+            </td>
+            <td>Decided that my notebook was too boring and single colored so this seemed like a good excuse to do something about it. Went with a few of my favorite things: Peter Pan, Converse shoes, and Space. It was fun to pull out my acryllic paints again. My gesso seems to have gone totally bad, but rather than go out and replace it, I just painted straight on the cover without any primer type layer. It worked fine for the most part, but I ran into a bit of trouble with some of my mixed colors being too transparent. Took a few layers to make those colors work, but it turned out alright.</td>
           </tr>
 
           <tr>
@@ -44,27 +70,13 @@ class OneHundredDaysOfContentPage extends Component {
           </tr>
 
           <tr>
-            <td>002</td>
-            <td>July 09, 2019</td>
-            <td>Acrylic Painting</td>
-            <td>
-              <a href="/assets/jordans-journal.jpeg">
-                <img src="/assets/jordans-journal.jpeg" className={componentStyles.jordansJournal} alt="Light blue journal with paintings of Peter Pan, a blue Converse shoe, a rocket ship and two planets" />
-              </a>
-            </td>
-            <td>Decided that my notebook was too boring and single colored so </td>
+            <td>000</td>
+            <td>July 07, 2019</td>
+            <td>Blog Post</td>
+            <td><a href="/blog/100-days-of-content/">#100DaysOfContent</a></td>
+            <td>Kicked off the challenge with a blog post that gathers up some ideas and goals to keep in mind as the days go by.</td>
           </tr>
-          <tr>
-            <td>003</td>
-            <td>July 10, 2019</td>
-            <td>SVG Illustration</td>
-            <td>
-              <a href="/assets/terrarium.png">
-                <img src="/assets/terrarium.png" alt="Graphic drawing of a few succulents in a bubble bowl" />
-              </a>
-            </td>
-            <td>Worked on a SVG of a succulent terrarium to add to the bookcase on my books list page. Want to add a bee that flies out of the bowl and around the page that is triggered by a mouse hover over. Maybe tomorrow's project.</td>
-          </tr>
+
         </table>
         
       </Layout>
